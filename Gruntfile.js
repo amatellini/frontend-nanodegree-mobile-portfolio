b1b6f36225f99dbec77c2cb34b9c285ef2f7a6d3
Andrea Matellini <andrea.matellini@gmail.com>
2017-02-12 16:36:30 +0100

    Added minify css grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,20 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    cssmin: {
+      target: {
+        files: {
+          //dest <- source
+          'css/style.min.css': ['css/style.css'],
+          'css/print.min.css': ['css/print.css']
+        }
+      }
+    },
+
   });
+
+  //load module and register grunt task
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('build', ['cssmin']);
 };
